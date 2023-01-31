39377617ded0019a55ddbd45dfeb81d27dfae881
Maik Lorenz <lorenz.maik@web.de>
2023-01-31 19:38:53 +0000

    Fix navbar transparency and sidenav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,32 +3,28 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
 import { useRouter } from "next/router";
 import ThemeSwitch from "./ThemeSwitch";
 
 const navItems = [
   ["Home", "/#main"],
-  ["About", "#about"],
-  ["Skills", "#skills"],
-  ["Projects", "#projects"],
+  ["About", "/#about"],
+  ["Skills", "/#skills"],
+  ["Projects", "/#projects"],
   ["Blog", "/blog"],
 ];
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
-  const [navBg, setNavBg] = useState("background");
-  const [linkColor, setLinkColor] = useState("skin-on-background");
+  const [navBg, setNavBg] = useState("bg-background");
   const router = useRouter();
 
   useEffect(() => {
     if (router.asPath.search("/projects") === 0) {
-      setNavBg("transparent");
-      setLinkColor("skin-on-background");
+      setNavBg("bg-transparent");
     } else {
-      setNavBg("background");
-      setLinkColor("skin-on-background");
+      setNavBg("bg-background");
     }
   }, [router]);
 
@@ -51,7 +47,7 @@ const Navbar = () => {
     <div
       className={`fixed ${
         shadow ? "shadow-xl shadow-shades" : ""
-      } fixed z-[100] w-full py-5 px-4 bg-${navBg}`}
+      } fixed z-[100] w-full ${navBg} py-5 px-4`}
     >
       <div className="flex h-full w-full items-center justify-between px-2 2xl:px-16">
         <div className="flex gap-8">
@@ -63,7 +59,7 @@ const Navbar = () => {
           <ThemeSwitch />
         </div>
         <div>
-          <ul className={`hidden md:flex text-${linkColor}`}>
+          <ul className={`hidden text-skin-on-background md:flex`}>
             {navItems.map(([title, url], idx) => (
               <Link href={url} key={idx} scroll={false}>
                 <li className="text-md border-b-edges ml-10 uppercase hover:border-b">
@@ -85,14 +81,16 @@ const Navbar = () => {
         <div
           className={
             nav
-              ? "bg-cultured fixed left-0 top-0 h-screen w-[75%] p-10 duration-500 ease-in sm:w-[60%] md:w-[45%]"
+              ? "fixed left-0 top-0 h-screen w-[75%] bg-surface p-10 duration-500 ease-in sm:w-[60%] md:w-[45%]"
               : "fixed -left-full top-0 p-10 duration-500 ease-in"
           }
         >
           <div>
             <div className="flex w-full items-center justify-between">
               <Link href="/">
-                <h1 className="font-monospace text-3xl">malorama</h1>
+                <h1 className="font-monospace text-3xl text-primary">
+                  malorama
+                </h1>
               </Link>
               <div
                 onClick={handleNav}
@@ -102,11 +100,7 @@ const Navbar = () => {
               </div>
             </div>
             <div className="my-4 border-b border-gray-300">
-              <p className="w-[85%] py-4 md:w-[90%]">
-                <strong>Ma</strong>ik <strong>Lor</strong>enz -{" "}
-                <strong>A</strong>sk <strong>M</strong>e <strong>A</strong>
-                nything!
-              </p>
+              <p className="w-[85%] py-4 md:w-[90%]">Maik Lorenz</p>
             </div>
           </div>
           <div className="flex flex-col py-4">
@@ -131,17 +125,19 @@ const Navbar = () => {
                   href="https://www.linkedin.com/in/maik-lorenz-5b7780bb/"
                   target="_blank"
                 >
-                  <div className="cursor-pointer rounded-full p-3 shadow-lg shadow-shades duration-300 ease-in hover:scale-105">
-                    <FaLinkedin size={24} color="#0e76a8" />
+                  <div className="cursor-pointer rounded-full bg-primary p-3 text-skin-on-primary shadow-lg shadow-shades duration-300 ease-in hover:scale-105">
+                    {/* color="#0e76a8" */}
+                    <FaLinkedin size={24} />
                   </div>
                 </Link>
                 <Link href="https://github.com/maikerlab" target="_blank">
-                  <div className="cursor-pointer rounded-full p-3 shadow-lg shadow-shades duration-300 ease-in hover:scale-105">
-                    <FaGithub color="#333" />
+                  <div className="cursor-pointer rounded-full bg-primary p-3 text-skin-on-primary shadow-lg shadow-shades duration-300 ease-in hover:scale-105">
+                    {/* color="#333" */}
+                    <FaGithub size={24} />
                   </div>
                 </Link>
                 <Link href="mailto:maik.lorenz@protonmail.com">
-                  <div className="cursor-pointer rounded-full p-3 shadow-lg shadow-shades duration-300 ease-in hover:scale-105">
+                  <div className="cursor-pointer rounded-full bg-primary p-3 text-skin-on-primary shadow-lg shadow-shades duration-300 ease-in hover:scale-105">
                     <AiOutlineMail size={24} />
                   </div>
                 </Link>
--- src/pages/projects/project1.tsx
+++ src/pages/projects/project1.tsx
@@ -1,59 +1,65 @@
 import React from "react";
 import test from "@assets/test.jpg";
 import Image from "next/image";
+import Head from "next/head";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
 
 const project1 = () => {
   return (
-    <div className="w-full">
-      <div className="relative h-[30vh] w-screen lg:h-[40vh]">
-        <div className="absolute top-0 left-0 z-10 h-[30vh] w-full bg-black/50 lg:h-[40vh]" />
-        <Image
-          src={test}
-          alt="/"
-          className="z-1 absolute"
-          fill={true}
-          style={{ objectFit: "cover" }}
-        />
-        <div className="absolute top-[70%] left-[50%] right-[50%] z-10 w-full max-w-[1240px] translate-x-[-50%] translate-y-[-50%] p-2 text-white">
-          <h2 className="py-2">My Webite</h2>
-          <h3 className="">Next.js / Tailwind CSS</h3>
-        </div>
-      </div>
-      <div className="mx-auto grid max-w-[1240px] gap-8 p-2 pt-8 md:grid-cols-5">
-        <div className="col-span-4">
-          <p>Project</p>
-          <h2 className="mb-4">Overview</h2>
-          <p className="mb-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-            id eligendi illo veritatis tempore voluptatem labore officia
-            expedita vitae laborum.
-          </p>
-          <button className="mt-4 mr-8 px-8 py-2">Demo</button>
-          <button className="mt-4 px-8 py-2">Code</button>
-        </div>
-        <div className="col-span-4 rounded-xl bg-surface p-4 shadow-xl shadow-shades md:col-span-1">
-          <div className="p-2">
-            <p className="pb-2 text-center font-bold">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1">
-              <p className="flex items-center py-2 text-gray-600 dark:text-gray-300">
-                <RiRadioButtonFill className="pr-1" /> React
-              </p>
-              <p className="flex items-center py-2 text-gray-600 dark:text-gray-300">
-                <RiRadioButtonFill className="pr-1" /> Tailwind CSS
-              </p>
-              <p className="flex items-center py-2 text-gray-600 dark:text-gray-300">
-                <RiRadioButtonFill className="pr-1" /> JavaScript
-              </p>
-            </div>
+    <>
+      <Head>
+        <title>Test Project | malorama</title>
+      </Head>
+      <div className="w-full">
+        <div className="relative h-[30vh] w-screen lg:h-[40vh]">
+          <div className="absolute top-0 left-0 z-10 h-[30vh] w-full bg-black/50 lg:h-[40vh]" />
+          <Image
+            src={test}
+            alt="/"
+            className="z-1 absolute"
+            fill={true}
+            style={{ objectFit: "cover" }}
+          />
+          <div className="absolute top-[70%] left-[50%] right-[50%] z-10 w-full max-w-[1240px] translate-x-[-50%] translate-y-[-50%] p-2 text-white">
+            <h2 className="py-2">My Webite</h2>
+            <h3 className="">Next.js / Tailwind CSS</h3>
           </div>
         </div>
-        <Link href="/#projects">
-          <p className="cursor-pointer underline">Back</p>
-        </Link>
+        <div className="mx-auto grid max-w-[1240px] gap-8 p-2 pt-8 md:grid-cols-5">
+          <div className="col-span-4">
+            <p>Project</p>
+            <h2 className="mb-4">Overview</h2>
+            <p className="mb-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
+              id eligendi illo veritatis tempore voluptatem labore officia
+              expedita vitae laborum.
+            </p>
+            <button className="mt-4 mr-8 px-8 py-2">Demo</button>
+            <button className="mt-4 px-8 py-2">Code</button>
+          </div>
+          <div className="col-span-4 rounded-xl bg-surface p-4 shadow-xl shadow-shades md:col-span-1">
+            <div className="p-2">
+              <p className="pb-2 text-center font-bold">Technologies</p>
+              <div className="grid grid-cols-3 md:grid-cols-1">
+                <p className="flex items-center py-2 text-gray-600 dark:text-gray-300">
+                  <RiRadioButtonFill className="pr-1" /> React
+                </p>
+                <p className="flex items-center py-2 text-gray-600 dark:text-gray-300">
+                  <RiRadioButtonFill className="pr-1" /> Tailwind CSS
+                </p>
+                <p className="flex items-center py-2 text-gray-600 dark:text-gray-300">
+                  <RiRadioButtonFill className="pr-1" /> JavaScript
+                </p>
+              </div>
+            </div>
+          </div>
+          <Link href="/#projects">
+            <p className="cursor-pointer underline">Back</p>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
