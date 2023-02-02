0d185a0c24962068146f51611383c4668f58e0fb
Maik Lorenz <lorenz.maik@web.de>
2023-02-02 18:45:43 +0000

    Add optional "caption" line to CodeHighlight

--- src/components/CodeHighlight.tsx
+++ src/components/CodeHighlight.tsx
@@ -7,9 +7,11 @@ import toast from "react-hot-toast";
 const CodeHighlight = ({
   language,
   codeString,
+  caption,
 }: {
   language: string;
   codeString: string;
+  caption?: string;
 }) => {
   const copyCode = () => {
     toast.success("Code was copied to clipboard", {
@@ -23,17 +25,24 @@ const CodeHighlight = ({
       <div className="flex flex-row justify-between text-gray-500">
         <p className="ml-2 text-sm italic">{language}</p>
         <button
-          className="flex items-center justify-center rounded-md p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
+          className="flex items-center justify-center rounded-md px-1 text-sm hover:bg-gray-200 dark:hover:bg-gray-700"
           onClick={copyCode}
         >
           <BiCopy className="mr-1" /> Copy
         </button>
       </div>
       <div className="rounded-xl pt-1">
-        <SyntaxHighlighter language={language} style={darcula}>
+        <SyntaxHighlighter
+          language={language}
+          style={darcula}
+          customStyle={{ margin: 0, padding: 0 }}
+        >
           {codeString}
         </SyntaxHighlighter>
       </div>
+      {caption && (
+        <p className="text-center text-sm italic text-gray-500">{caption}</p>
+      )}
     </div>
   );
 };
